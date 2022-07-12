f6dfa0140c02c0a91c3e246a8e50bdf80d6e6b3a
Paulo Salvatore <salvatoregames@gmail.com>
2022-07-12 12:00:34 -0300

    Criação do endpoint `/oi` e `[GET] /herois` (Read All)

--- index.js
+++ index.js
@@ -5,4 +5,21 @@ app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
-app.listen(3000);
+// /oi -> "Olá, mundo"
+app.get("/oi", function (req, res) {
+  res.send("Olá, mundo");
+});
+
+// Endpoints de Heróis
+
+const herois = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
+//               0                   1                2
+
+// [GET] /herois
+app.get("/herois", function (req, res) {
+  res.send(herois);
+});
+
+app.listen(3000, function () {
+  console.log("Aplicação rodando em http://localhost:3000");
+});
