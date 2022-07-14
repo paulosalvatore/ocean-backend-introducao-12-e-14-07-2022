775e5d52081d2408c2dc2705a511b681fb4934c6
Paulo Salvatore <salvatoregames@gmail.com>
2022-07-14 11:51:10 -0300

    Endpoint de Create e Read By Id

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+// Registrar que estamos usando JSON no Body
+// da requisição
+app.use(express.json());
+
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
@@ -15,9 +19,35 @@ app.get("/oi", function (req, res) {
 const herois = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
 //               0                   1                2
 
-// [GET] /herois
+// [GET] /herois -> Read All (Ler tudo)
 app.get("/herois", function (req, res) {
   res.send(herois);
+});
+
+// [GET] /herois/:id -> Read by ID (Ler pelo ID)
+app.get("/herois/:id", function (req, res) {
+  // Pegamos o ID pela rota
+  const id = req.params.id;
+
+  // Acessar o registro na lista, usando o ID
+  const item = heois[id - 1];
+
+  // Enviar o registro encontrado
+  res.send(item);
+});
+
+// [POST] /herois -> Create (Criar)
+app.post("/herois", function (req, res) {
+  // console.log(req.body);
+
+  // Acessamos o valor que foi enviado na request
+  const item = req.body.nome;
+
+  // Insere esse valor na lista
+  herois.push(item);
+
+  // Exibe uma mensagem de sucesso
+  res.send("Item criado com sucesso!");
 });
 
 app.listen(3000, function () {
